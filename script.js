62accb89f76339f6b067cfde54dc16464b5c80aa
Benjamin Trumpy <trumpy.b@stu.janesville.k12.wi.us>
2023-05-30 13:22:13 -0500

    Update

---
+++ script.js
@@ -0,0 +1,13 @@
+function light(){
+    document.getElementById("body").style.color="black"
+    document.getElementById("body").style.background="lightgreen"
+    document.getElementById('nav').style.background='darkgreen'
+    document.getElementById('nav').style.color='white'
+}
+function dark(){
+    document.getElementById("body").style.color="white"
+    document.getElementById("body").style.background="darkgreen"
+    document.getElementById('nav').style.background='lightgreen'
+    document.getElementById('nav').style.color='black'
+}
+dark()
