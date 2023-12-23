f86e655303bcc1963dde6fb1acb1a174e372d13c
trumpyb <91992867+trumpyb@users.noreply.github.com>
2023-12-22 21:53:42 -0600

    Create save_html.php

---
+++ website/save_html.php
@@ -0,0 +1,14 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $websiteUrl = $_POST["websiteUrl"];
+
+    // Fetch HTML content
+    $htmlContent = file_get_contents($websiteUrl);
+
+    // Save HTML content to a text file
+    $filename = "saved_html.txt";
+    file_put_contents($filename, $htmlContent);
+
+    echo "HTML content saved successfully in $filename";
+}
+?>
